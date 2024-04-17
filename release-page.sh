a653e65657f37f418ef94738d33130aca22cb622
zhiqiangwang <zhiqiangwang@aliyun.com>
2024-04-17 13:21:43 +0800

    update sh

--- release-page.sh
+++ release-page.sh
@@ -18,23 +18,29 @@ git config --global user.name "github-actions[bot]"
 git config --global user.email  "41898282+github-actions[bot]@users.noreply.github.com"
 echo machine github.com login $GITHUB_USER password $GITHUB_TOKEN > ~/.netrc
 
-echo "Updating code repository in progress..."
-echo "Package and publish nodes"
-git clone https://github.com/drawdb-io/drawdb.git code
+# echo "Updating code repository in progress..."
+# echo "Package and publish nodes"
+# git clone https://github.com/drawdb-io/drawdb.git code
 cd code
-npm ci
+# npm ci
 npm run build
 
+# gh-pages分支存在的情况
 git clone -b gh-pages https://github.com/myhosts/drawdb-io.git gh-pages
 echo ".git directory transfer"
 mv gh-pages/.git dist
 
 cd dist
+
+# gh-pages分支不存在的时候
 # git init
+# git push https://github.com/myhosts/drawdb-io.git gh-pages  --force
+
 echo 'gh.drawdb.vhosts.top' > CNAME
+echo "$today_commit" > README.md
 git add -A
 git commit -m "deploy commit $today_commit"
-git push -f https://github.com/myhosts/drawdb-io.git main:gh-pages
+git push -f origin gh-pages
 
 cd ..
 cd ..
@@ -43,4 +49,4 @@ echo "main branch Version update"
 echo "$today_commit" >currentcommit
 git add currentcommit
 git commit -a -m "Automatically update version to  $today_commit"
-git push -f https://github.com/myhosts/drawdb-io.git main
+git push -f origin main
